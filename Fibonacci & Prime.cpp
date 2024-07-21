97ec20302530798a79dfb8faf1c069ba73777279
VedSojitra <136689611+VedSojitra@users.noreply.github.com>
2024-07-21 14:33:52 +0530

    Fibonacci & Prime.cpp

---
+++ Fibonacci & Prime.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // // Fibonacci series
+
+    // int n;
+    // cout<<"Enter the number ";
+    // cin>>n;
+
+    // int a = 0, b = 1;
+    // cout<< a << " " <<b<<" ";
+
+    // for(int i = 1; i<=n; i++){
+    //     int nextNum = a+b;
+    //     cout<< nextNum<<" ";
+
+    //     a = b;
+    //     b = nextNum;
+    // }
+
+    
+
+    // // Prime number
+
+    // int p;
+    // cout<<"Enter the number ";
+    // cin>>p;
+    // bool isPrime = 1;
+
+    // for(int i=2; i<p; i++){
+    //     if(p%i==0){
+    //         isPrime = 0;
+    //         break;
+    //     }
+    // }
+
+    // if(isPrime==0){
+    //     cout<<"The number is not a prime number"<<endl;
+    // }
+    // else{
+    //     cout<<"The number is a prime number"<<endl;
+    // }
+
+}
