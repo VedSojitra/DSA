14fcfb2af5f0104f55b93933d7aa642221a004f3
Ved Sojitra <136689611+VedSojitra@users.noreply.github.com>
2024-09-03 20:42:35 +0530

    Recursion - 1.cpp

--- Recursion - 1.cpp
+++ Recursion - 1.cpp
@@ -20,14 +20,60 @@ int factorial(int n){
     
 }
 
+int power(int n){
+
+    //base case
+    if(n == 0){
+        return 1;
+    }
+
+    //recursive relation
+    // 2^3 = 2 * 2^2
+    // f(n) = 2 * f(n-1)
+    // biggerProblem = 2 * smallerProblem
+
+    int smallerProblem = power(n-1);
+    int biggerProblem = 2 * smallerProblem;
+
+    return biggerProblem;
+
+    // return 2 * power(n-1);
+
+}
+
+void print(int n){
+    // base case
+    if(n == 0){
+        return ;
+    }
+
+    //recursive relation
+    // 5 = 5-1
+    // f(n) = f(n-1)
+
+    cout << n << " ";
+
+    print(n-1);
+    cout << endl;
+    cout << n << " ";
+
+}
 
 int main(){
     int n;
+    cout << "Enter a number: " << endl;
     cin >> n;
 
     int ans = factorial(n);
 
     cout << "Factorial of "<< n << " is " << ans << endl;
+
+    int ans1 = power(n);
+
+    cout << "Power of 2 raise to " << n << " is " << ans1 << endl;
+
+    cout << "Printing backword :- ";
+    print(n);
 
     return 0;
 }
