3a229309ef8a036951930de551fde15eb9d7120b
Ved Sojitra <136689611+VedSojitra@users.noreply.github.com>
2024-09-05 10:48:02 +0530

    Recursion - 2.cpp

--- Recursion - 2.cpp
+++ Recursion - 2.cpp
@@ -13,7 +13,28 @@ int CountStairs(int nstairs){
     return ans;
 }
 
+void SayDigit(int n, string arr[]){
+    // base case
+    if(n == 0){
+        return ;
+    }
+
+    //processes
+    int digit = n % 10;
+    n = n / 10;
+
+    // recursive call
+    SayDigit(n, arr);
+    cout << arr[digit] << " ";
+}
+
 int main(){
     int nstairs = 6;
     cout << CountStairs(nstairs) << endl;
+
+    string arr[10] = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+    cout << "Enter any digit between 0 to 9: ";
+    int n;
+    cin >> n;
+    SayDigit(n,arr);
 }
