dbbd04cc0a78a1a7f4f0dbd13d9a87fa05924668
Ved Sojitra <136689611+VedSojitra@users.noreply.github.com>
2024-10-02 09:54:17 +0530

    OOPS-1.cpp

---
+++ OOPS-1.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+
+/*
+=> Polymorphism:- Many forms
+=> Two type:-
+
+1. Compile time polymorphism:- 
+- Function overloading
+- Operator overloading
+
+2. Runtime polymorphism(Dynamic Polymorphism):-
+- Function overriding:- Performed only by inheritance
+                     :- Methof of parent class and child class must have same name and same parameters
+
+
+
+=> Abstraction:- Implementation hiding
+-> Abstraction in classes can be achieved by using access modifiers
+*/
+
+
+// Compile time(Overloading):-
+class A {
+
+    public:
+    void sayHello() {
+        cout << "Hello" << endl;
+    }
+    
+    void sayHello(char name) {
+        cout << "Hello hey" << endl;
+    }
+
+    int sayHello(string name) {
+        cout << "Hey " << name  << endl;
+        return 1;
+    }
+
+};
+
+class B {
+    public:
+    int a;
+    int b;
+
+    public: 
+    int add() {
+        return a+b;
+    }
+
+    void operator+ (B &obj) {
+        /*int value1 = this -> a;
+        int value2 = obj.a;
+        cout << "output " << value2 - value1 << endl; 
+        */
+       cout << "Hello instead of + operator" << endl;
+    }
+
+    void operator() () {
+        cout << "Inside Bracket" << this->a << endl;
+    }
+
+};
+
+
+int main(){
+    // Overloading
+    A obj;
+    obj.sayHello();
+
+
+
+    B obj1, obj2;
+
+    obj1.a = 4;
+    obj2.a = 7;
+
+    obj1 + obj2;
+    obj1();
+
+}
