e972d87a4c1feb092b42c8642044db415d89ba5e
Ved Sojitra <136689611+VedSojitra@users.noreply.github.com>
2024-11-25 16:15:58 +0530

    DoublyLL.cpp

---
+++ DoublyLL.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    //constructor
+    Node(int d){
+        this -> data = d;
+        this -> next = NULL;
+        this -> prev = NULL;
+    }
+
+    //destructor
+    ~Node(){
+        int value = this -> data;
+        if(this -> next != NULL){
+            this -> next = NULL;
+            delete next;
+        }
+        cout << "memory is free for node with data " << value << endl; 
+    }
+};
+
+void print(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+void insertAtHead(Node* &head, int d){
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head -> prev = temp;
+    head = temp;
+}
+
+void inseartAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    temp -> prev = tail;
+    tail = temp;
+}
+
+void insertAtPosition(Node* &tail, Node* &head, int position, int d){
+    // insert at start
+    if(position == 1){
+        insertAtHead(head,d);
+        return ;
+    }
+
+    Node* temp = head;
+    int cnt = 1;
+    while(cnt < position - 1){
+        temp = temp -> next;
+        cnt++;
+    }
+
+    // insert at last
+    if(temp -> next == NULL){
+        inseartAtTail(tail,d);
+        return ;
+    }
+
+    // create a node
+    Node* nodeToInsert = new Node(d);
+    
+    nodeToInsert -> next = temp -> next;
+    temp -> next -> prev = nodeToInsert;
+    temp -> next = nodeToInsert;
+    nodeToInsert -> prev = temp;
+}
+
+int main(){
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    Node* tail = node1;
+    
+    print(head);
+
+    insertAtHead(head,9);
+    insertAtHead(head,8);
+    print(head);
+
+    inseartAtTail(tail,11); 
+    inseartAtTail(tail,12);
+    print(head);
+
+    insertAtPosition(tail, head, 3, 101);
+    insertAtPosition(tail, head, 1, 7);
+    insertAtPosition(tail, head, 8, 13);
+    print(head);
+
+    return 0;
+}
