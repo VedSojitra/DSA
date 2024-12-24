16df1b3a0468a93ff2f597ef8f8bab2bf1f4f036
Ved Sojitra <136689611+VedSojitra@users.noreply.github.com>
2024-12-24 10:46:19 +0530

    Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -41,12 +41,36 @@ class Queue{
         }
         return arr[rear];
     }
+
+    void enqueue(int val){
+        if(isFull()){
+            cout << "Queue is Full" << endl;
+            return;
+        }
+
+        if(isEmpty()) front = 0;
+
+        rear++;
+        arr[rear] = val;
+    }
 };
 
 int main() {
-  Queue q (5);
-  if(q.isEmpty())
-    cout << "Queue is Empty" << endl;
+    Queue q (5);
+    if(q.isEmpty())
+        cout << "Queue is Empty" << endl;
 
-  return 0;
+    // Enqueueing
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+
+    // Displaying status of the queue after enqueuing
+    cout << "\nAfter Enqueueing:" << endl;
+
+    cout << "Front element: " << q.getFront() << endl;
+    cout << "Rear element: " << q.getRear() << endl;
+
+    
+    return 0;
 }
