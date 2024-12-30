1d377cbe975df3df0dab7ad1e3821d95997a5231
Ved Sojitra <136689611+VedSojitra@users.noreply.github.com>
2024-12-30 21:07:46 +0530

    CircularQueue.cpp

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -25,4 +25,31 @@ class CircularQueue{
         if((rear + 1) % size == front) return true;
         else return false;
     }
+
+    void enqueue(int value) {
+        if (isFull()) {
+            cout << "Circular Queue is full!" << endl;
+            return;
+        }
+        if (isEmpty()) {
+            front = 0;  // If queue is empty, set front to 0
+        }
+        rear = (rear + 1) % size;
+        arr[rear] = value;
+        cout << value << " enqueued to the Circular Queue." << endl;
+    }
+
+   
+    void dequeue() {
+        if (isEmpty()) {
+            cout << "Circular Queue is empty!" << endl;
+            return;
+        }
+        cout << arr[front] << " dequeued from the Circular Queue." << endl;
+        if (front == rear) {
+            front = rear = -1;  // Reset the queue if last element is dequeued
+        } else {
+            front = (front + 1) % size;
+        }
+    }
 }
