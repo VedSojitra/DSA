a4e481c73dce7954f70e7fc7d72e534d7dba0526
Ved Sojitra <136689611+VedSojitra@users.noreply.github.com>
2025-01-03 23:29:44 +0530

    BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -62,6 +62,36 @@ void levelOrderTraversal(node* root){
     }
 }
 
+void inorder(node* root){
+    if(root == NULL){
+        return ;
+    }
+
+    inorder(root -> left);
+    cout << root -> data << " ";
+    inorder(root -> right);
+}
+
+void preorder(node* root){
+    if(root == NULL){
+        return ;
+    }
+
+    cout << root -> data << " ";
+    preorder(root -> left);
+    preorder(root -> right);
+}
+
+void postorder(node* root){
+    if(root == NULL){
+        return ;
+    }
+
+    postorder(root -> left);
+    postorder(root -> right);
+    cout << root -> data << " ";
+}
+
 int main(){
     node* root = NULL;
 
