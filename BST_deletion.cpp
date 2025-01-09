f709f8ed88046c51e523b2801a827f92dd591027
Ved Sojitra <136689611+VedSojitra@users.noreply.github.com>
2025-01-09 18:54:01 +0530

    BST_deletion.cpp

--- BST_deletion.cpp
+++ BST_deletion.cpp
@@ -72,6 +72,63 @@ void inorder(Node* root){
     inorder(root -> right);
 }
 
+Node* minValue(Node* root){
+    Node* temp = root;
+
+    while(temp->left != NULL){
+        temp = temp -> left;
+    }
+    return temp;
+}
+
+Node* deleteFromBST(Node* root, int val){
+    if(root == NULL){
+        return root;
+    }
+
+    if(root->data == val){
+        // 0 child
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            return NULL;
+        }
+
+        // 1 child
+        // For left child
+        if(root->left != NULL && root->right == NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // For right child
+        if(root->right != NULL && root->left == NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        // 2 child
+        if(root->left != NULL && root->right != NULL){
+            int mini = minValue(root->right) -> data;
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+
+    }
+    else if(root->data > val){
+        // traverse in left
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else{
+        // traverse in right
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
+
 void takeInput(Node* &root){
     int data;
     cin >> data;
@@ -91,6 +148,16 @@ int main(){
     takeInput(root);
 
     cout << "Printing the BST" << endl;
+    levelOrderTraversal(root);
+
+    cout << "Inorder traversal is: ";
+    inorder(root);
+
+
+    // Deletion
+    root = deleteFromBST(root, 90);
+
+    cout << endl << "Printing the BST" << endl;
     levelOrderTraversal(root);
 
     cout << "Inorder traversal is: ";
